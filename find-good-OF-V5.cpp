3d2d43a8648aac75140167e249a149e1b729b300
suazung <suaone01@gmail.com>
2024-07-03 15:23:10 +0700

    Update find-good-OF-V5.cpp

--- find-good-OF-V5.cpp
+++ find-good-OF-V5.cpp
@@ -2,34 +2,56 @@
 #include "sierrachart.h"
 #include <vector>            // For using std::vector
 #include <sstream>           // For string manipulation
+#include <iomanip>
 
 
 #define CHECK_5LEVEL 1 
 #define CHECK_5LEVEL_AND_POCATBOTTOM 2 
 #define CHECK_5LEVEL_AND_POCATBOTTOM_AND_UPCANDLE 3
 #define CHECK_5LEVEL_AND_POCATTOP_AND_UPCANDLE 4  
-#define MAX_VECTOR_SIZE 500
+#define MAX_VECTOR_SIZE 1000
 
 int uniqueNumber = 7889 ;
 
 // Structure to represent an good buy OF start bar
 struct st_GoodBuyOFStartBar {
     int index;
-    SCDateTime time;
-    float volume;
-	int lineNumber;
+   
+	st_GoodBuyOFStartBar(int idx) : index(idx) {}
+};
+
+// Structure to represent an good buy Orderflow
+struct st_GoodBuyOF {
+    int startBarIndex;
+	int endBarIndex;
+	int volume_of_start_bar ;
+	int delta_of_start_bar ;
+	int volume_of_end_bar ;
+	int delta_of_end_bar ;
+    SCDateTime time_of_start_bar ;
+	SCDateTime time_of_end_bar ;
+    int lineNumber;
   
-	st_GoodBuyOFStartBar(int idx, SCDateTime t, float v , int ln) : index(idx), time(t), volume(v) , lineNumber(ln) {}
+	st_GoodBuyOF(int idx1, int v1 , int d1  , SCDateTime t1 ,
+	             int idx2, int v2 , int d2  , SCDateTime t2 ,
+				 int ln ) 
+				 : startBarIndex(idx1), volume_of_start_bar(v1), delta_of_start_bar(d1) , time_of_start_bar(t1) ,
+				   endBarIndex(idx2)  , volume_of_end_bar(v2)  , delta_of_end_bar(d2)   , time_of_end_bar(t2)   ,
+				   lineNumber(ln) {}
+	
 };
 
 int isThisStartBarOFGoodBuyOrderflow(SCStudyInterfaceRef sc , int startIndex);
 
 bool checkValidBar(SCStudyInterfaceRef sc , int idx , int condition);
 
-void findGoodBuyOrderFlow(SCStudyInterfaceRef sc, std::vector<st_GoodBuyOFStartBar>& detectedGoodBuyStartBars);
+void findGoodBuyOrderFlow(SCStudyInterfaceRef sc, 
+                          std::vector<st_GoodBuyOFStartBar>& detectedGoodBuyStartBars , 
+						  std::vector<st_GoodBuyOF>& detectGoodBuyOF);
 
 //std::vector<st_GoodBuyOFStartBar> detectedGoodBuyStartBars;
 
+float findMaxGoUpBeforeBreakLow(SCStudyInterfaceRef sc ,int indexOfStartBar , int indexOfEndtBar) ;
 
 SCDLLName("SUA FIND GOOD BUY OF")
 
@@ -82,6 +104,15 @@ SCSFExport scsf_FindGoodBuyOF(SCStudyInterfaceRef sc)
         sc.SetPersistentPointer(1, detectedGoodBuyStartBars);
     }
 	
+	auto detectGoodBuyOF = static_cast<std::vector<st_GoodBuyOF>*>(sc.GetPersistentPointer(2));
+	
+	if (!detectGoodBuyOF) {
+        detectGoodBuyOF = new std::vector<st_GoodBuyOF>();
+        sc.SetPersistentPointer(2, detectGoodBuyOF);
+    }
+	
+	
+	
 	
 	int minVol = i_minVol.GetInt();
 	int startIndex = sc.UpdateStartIndex;
@@ -94,13 +125,13 @@ SCSFExport scsf_FindGoodBuyOF(SCStudyInterfaceRef sc)
 	if (sc.IsFullRecalculation || sc.LastCallToFunction || sc.HideStudy)
 	{
 		
-		for (int i = 0; i < detectedGoodBuyStartBars->size(); i++)
+		for (int i = 0; i < detectGoodBuyOF->size(); i++)
 		{
-			sc.DeleteUserDrawnACSDrawing(sc.ChartNumber, detectedGoodBuyStartBars->at(i).lineNumber);
+			sc.DeleteUserDrawnACSDrawing(sc.ChartNumber, detectGoodBuyOF->at(i).lineNumber);
 		}
 		
 		// Drawings removed, now clear to avoid re-drawing them again
-		detectedGoodBuyStartBars->clear();
+		detectGoodBuyOF->clear();
 
 			
 		// Study is being removed nothing more to do
@@ -116,10 +147,13 @@ SCSFExport scsf_FindGoodBuyOF(SCStudyInterfaceRef sc)
 		detectedGoodBuyStartBars->clear();
 	
 	
+	if (detectGoodBuyOF != NULL)
+		detectGoodBuyOF->clear();
+	
 	
 	
 	 // 1.Loop through bars to detect good buy start bar pattern
-    for (int i = startIndex; i < sc.ArraySize; ++i) 
+    for (int i = startIndex; i < sc.ArraySize-1; ++i) 
 	{
 		
 		// Check if this is a new bar
@@ -135,14 +169,9 @@ SCSFExport scsf_FindGoodBuyOF(SCStudyInterfaceRef sc)
 		// if found good buy start bar , then add it to vector
 		if(isItStartBarOFGoodBuyOrderflow)
 		{
-			SCDateTime time = sc.BaseDateTimeIn[i];
-            float vol = sc.Volume[i];
-            int lineNm = uniqueNumber + i;
-			
+						
 			// use emplace_back
-			detectedGoodBuyStartBars->emplace_back(i, time, vol , lineNm);
-
-			
+			detectedGoodBuyStartBars->emplace_back(i);			
 			
 			// Ensure the vector does not exceed the maximum size
 			if (detectedGoodBuyStartBars->size() > MAX_VECTOR_SIZE) {
@@ -152,44 +181,91 @@ SCSFExport scsf_FindGoodBuyOF(SCStudyInterfaceRef sc)
 		}
 		
 		// 2. find end bar of good buy OF
-		//findGoodBuyOrderFlow(sc , *detectedGoodBuyStartBars);
+		//findGoodBuyOrderFlow(sc , *detectedGoodBuyStartBars , *detectGoodBuyOF);
 		
 	
 	}
 	
 	 // 2. find end bar of good buy OF
-	findGoodBuyOrderFlow(sc , *detectedGoodBuyStartBars);
+	findGoodBuyOrderFlow(sc , *detectedGoodBuyStartBars , *detectGoodBuyOF);
 	
-	SCString filePath = sc.DataFilesFolder() + "CollectedData.txt";
+	
+	// 3. draw rectangle of good buy OF
+	for (int i = 0; i < detectGoodBuyOF->size(); i++)
+	{
+		
+		// **************************** Draw GOOD BUY OF HERE *********************************************
+		s_UseTool rectangle;
+		rectangle.Clear();
+		rectangle.ChartNumber = sc.ChartNumber;
+		rectangle.DrawingType = DRAWING_RECTANGLEHIGHLIGHT;
+		rectangle.AddAsUserDrawnDrawing = 1;
+		rectangle.BeginIndex = detectGoodBuyOF->at(i).startBarIndex;
+		rectangle.EndIndex = detectGoodBuyOF->at(i).endBarIndex;
+		rectangle.BeginValue = sc.High[detectGoodBuyOF->at(i).endBarIndex];
+		rectangle.EndValue = sc.Low[detectGoodBuyOF->at(i).startBarIndex];
+		rectangle.Color = RGB(255, 0, 0);
+		rectangle.SecondaryColor = RGB(255, 255, 0);  // Yellow color
+		rectangle.LineWidth = 1;
+		rectangle.TransparencyLevel = 75;
+		rectangle.AddMethod = UTAM_ADD_OR_ADJUST;
+		rectangle.LineNumber = detectGoodBuyOF->at(i).lineNumber ;
+		sc.UseTool(rectangle);
+						
+		SCString msg;
+		msg.Format("start bar : %d | end bar : %d \n" , detectGoodBuyOF->at(i).startBarIndex , detectGoodBuyOF->at(i).endBarIndex );
+		sc.AddMessageToLog(msg,0);
+	}
+	
+	
+	if(startIndex == 0)
+	{
+		SCString filePath = sc.DataFilesFolder() + "CollectedData.txt";
 
-    // Open the file in append mode
-    std::ofstream outputFile;
-    if (sc.Index == 0)
-    {
-        // If it's the first bar, overwrite the file
-        outputFile.open(filePath.GetChars(), std::ios::out);
-    }
-    else
-    {
-        // Otherwise, append to the file
-        outputFile.open(filePath.GetChars(), std::ios::app);
-    }
+		// Open the file in append mode
+		std::ofstream outputFile;
+		if (sc.Index == 0)
+		{
+			// If it's the first bar, overwrite the file
+			outputFile.open(filePath.GetChars(), std::ios::out);
+		}
+		else
+		{
+			// Otherwise, append to the file
+			outputFile.open(filePath.GetChars(), std::ios::app);
+		}
 
-    // Check if the file opened successfully
-    if (!outputFile.is_open())
-    {
-        SCString error;
-        error.Format("Failed to open file: %s", filePath.GetChars());
-        sc.AddMessageToLog(error, 1);
-        return;
-    }
+		// Check if the file opened successfully
+		if (!outputFile.is_open())
+		{
+			SCString error;
+			error.Format("Failed to open file: %s", filePath.GetChars());
+			sc.AddMessageToLog(error, 1);
+			return;
+		}
 
-    // Write the data to the file
-    outputFile << "Bar Index: " << "\n";
+		for (int i = 0; i < detectGoodBuyOF->size(); i++)
+		{
+			
+			SCString date = sc.DateTimeToString( detectGoodBuyOF->at(i).time_of_start_bar, FLAG_DT_COMPLETE_DATETIME);
+			float howMuchMove = findMaxGoUpBeforeBreakLow(sc,detectGoodBuyOF->at(i).startBarIndex , detectGoodBuyOF->at(i).endBarIndex) ;
+			int hmm = sc.PriceValueToTicks(howMuchMove) ;
 
-    // Close the file
-    outputFile.close();
-
+			
+			// Write the data to the file
+			outputFile << "start Index: " << detectGoodBuyOF->at(i).startBarIndex ;
+			outputFile << " , end Index: " << detectGoodBuyOF->at(i).endBarIndex ;
+			outputFile << " , max Price move before SL: " /*<< std::setprecision(4) */<< hmm ;
+			outputFile << " , time : " << date << "\n";
+		}
+				  
+		// Close the file
+		outputFile.close();
+		
+	}
+	
+	
+	
 	
 	
 }
@@ -299,10 +375,10 @@ bool checkValidBar(SCStudyInterfaceRef sc , int idx , int condition)
 }
 
 
-void findGoodBuyOrderFlow(SCStudyInterfaceRef sc , std::vector<st_GoodBuyOFStartBar>& detectedGoodBuyStartBars) 
+void findGoodBuyOrderFlow(SCStudyInterfaceRef sc , std::vector<st_GoodBuyOFStartBar>& detectedGoodBuyStartBars , std::vector<st_GoodBuyOF>& detectGoodBuyOF) 
 {
     // loop from first index of start bar of good buy OF to last 
-	for (/*const*/ auto& bar : detectedGoodBuyStartBars) {
+	for (const auto& bar : detectedGoodBuyStartBars) {
         int startBarIndex = bar.index;
 		// loop from start bar to current bar
         for (int j = startBarIndex + 1; j < sc.ArraySize - 1; ++j) 
@@ -326,8 +402,26 @@ void findGoodBuyOrderFlow(SCStudyInterfaceRef sc , std::vector<st_GoodBuyOFStart
 					// draw if no candle break low of start bar
 					if(noCandlebreakLow)
 					{
-						bar.lineNumber = uniqueNumber + startBarIndex + j ;
+						int idx1 = startBarIndex;
+						int v1 = sc.Volume[startBarIndex];
+						int d1 = 0;
+						SCDateTime t1 = sc.BaseDateTimeIn[startBarIndex];
 						
+						int idx2 = j ;
+						int v2 = sc.Volume[j];
+						int d2 = 0;
+						SCDateTime t2 = sc.BaseDateTimeIn[j];	
+
+                        int ln = uniqueNumber + startBarIndex + j ;						
+						
+											
+						//auto& data = detectedGoodBuyStartBars;
+						detectGoodBuyOF.emplace_back(idx1, v1, d1 , t1 ,idx2 ,v2 , d2 , t2 , ln );
+												
+						/*
+						//bar.lineNumber = uniqueNumber + startBarIndex + j ;	
+						
+						// **************************** Draw GOOD BUY OF HERE *********************************************
 						s_UseTool rectangle;
 						rectangle.Clear();
 						rectangle.ChartNumber = sc.ChartNumber;
@@ -342,18 +436,55 @@ void findGoodBuyOrderFlow(SCStudyInterfaceRef sc , std::vector<st_GoodBuyOFStart
 						rectangle.LineWidth = 1;
 						rectangle.TransparencyLevel = 75;
 						rectangle.AddMethod = UTAM_ADD_OR_ADJUST;
-						rectangle.LineNumber = bar.lineNumber ;
+						rectangle.LineNumber = ln ;
 						sc.UseTool(rectangle);
 						
 						SCString msg;
 						msg.Format("start bar : %d | end bar : %d \n" , startBarIndex , j );
 						sc.AddMessageToLog(msg,0);
+						
+						*/
 					}
                  
                 }
             }
         }
     }
+}
+
+float findMaxGoUpBeforeBreakLow(SCStudyInterfaceRef sc ,int indexOfStartBar , int indexOfEndtBar) 
+{
+	float low = sc.Low[indexOfStartBar] - 0.0001 ;
+	
+	int indexThatBreakLow = -1;
+	
+	// find which index break low
+	for(int i = indexOfEndtBar ; i < sc.ArraySize-1 ; i++)
+	{
+		if( sc.Low[i] <= low )
+		{
+			indexThatBreakLow = i;
+			break;
+		}
+	}
+	
+	if(indexThatBreakLow == -1)
+	{
+		indexThatBreakLow = sc.ArraySize-1;
+	}
+	
+	float maxPriceGo = 0;
+	
+	for(int i = indexOfEndtBar ; i <= indexThatBreakLow ; i++)
+	{
+		if(sc.High[i] > maxPriceGo)
+		{
+			maxPriceGo = sc.High[i] ;
+		}
+	}
+	
+	return maxPriceGo-sc.High[indexOfEndtBar]  ;
+	
 }
 
 
